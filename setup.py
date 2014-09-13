02f734b899ff5d8b7bc53268414417cad266cb1b
Todd Sifleet <todd.sifleet@gmail.com>
2014-09-13 18:04:35 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     description = 'Python retry utility',
     name =  'retry',
-    version = '1.0',
+    version = '0.0.01',
     author='Todd Siflet',
     author_email='todd.siflet@gmail.com',
     py_modules = ['retry.retry'],
