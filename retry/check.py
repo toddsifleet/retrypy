0e20776c571f17176de2c4046205c9979444a11c
Todd Sifleet <todd.sifleet@gmail.com>
2014-09-15 23:20:17 -0700

    user str not e.message

--- retry/check.py
+++ retry/check.py
@@ -2,15 +2,15 @@ import re
 
 
 def message_equals(check_string):
-    return lambda e, n: check_string == e.message
+    return lambda e, n: check_string == str(e)
 
 
 def message_contains(check_string):
-    return lambda e, n: check_string in e.message
+    return lambda e, n: check_string in str(e)
 
 
 def message_matches(regex):
     if not isinstance(input, re._pattern_type):
         regex = re.compile(regex)
 
-    return lambda e, n: regex.search(e.message)
+    return lambda e, n: regex.search(str(e))
