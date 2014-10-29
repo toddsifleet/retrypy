624ed42b617874562904f1c63ef68d033f1f8d5b
Todd Sifleet <todd.sifleet@gmail.com>
2014-10-28 21:04:43 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
+
+import retrypy
+
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(
-    description = 'Python retry utility',
-    name =  'retrypy',
-    version = '0.0.01',
+    description='Python retry utility',
+    long_description=long_description,
+    name='retrypy',
+    version=retrypy.__version__,
     author='Todd Siflet',
     author_email='todd.siflet@gmail.com',
-    py_modules = ['retrypy.retry'],
+    py_modules=['retrypy.retry'],
 )
